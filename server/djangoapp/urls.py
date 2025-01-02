a67ee5f9a2b167bc8626337ce4f1d78cc1a7dac8
Reinjka <reinisandzans1@gmail.com>
2025-01-02 12:34:17 -0500

    Module3,3

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -7,6 +7,11 @@ app_name = 'djangoapp'
 
 urlpatterns = [
     path('get_cars', views.get_cars, name='get_cars'),
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+    path(route='add_review', view=views.add_review, name='add_review'),
     path('login', views.login_user, name='login'),
     path('logout', views.logout_request, name='logout'),
     path('register', views.registration_request, name='register'),
