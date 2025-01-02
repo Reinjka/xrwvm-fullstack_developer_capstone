f35a88cb7ad86ac9db70e5894a01f56f842e81f8
Reinjka <reinisandzans1@gmail.com>
2025-01-02 11:03:48 -0500

    Module3,2

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,23 +1,13 @@
-# Uncomment the imports before you add the code
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
 
 app_name = 'djangoapp'
+
 urlpatterns = [
-    # # path for registration
-    # Path for registration
-    path(route='register', view=views.registration_request, name='register'),
-
-    # path for login
-    path(route='login', view=views.login_user, name='login'),
-
-    # Path for logout
-    path(route='logout', view=views.logout_request, name='logout'),
-
-    # path for dealer reviews view
-
-    # path for add a review view
-
+    path('get_cars', views.get_cars, name='get_cars'),
+    path('login', views.login_user, name='login'),
+    path('logout', views.logout_request, name='logout'),
+    path('register', views.registration_request, name='register'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
