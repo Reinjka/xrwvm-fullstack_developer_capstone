a67c913796805f04c67ed95f3bdafda4b5c6c059
Reinjka <reinisandzans1@gmail.com>
2025-01-03 10:42:40 -0500

    stuck

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,6 +1,8 @@
 import LoginPanel from "./components/Login/Login";
 import RegisterPanel from "./components/Register/Register";
 import { Routes, Route } from "react-router-dom";
+import Dealers from './components/Dealers/Dealers';
+import Dealer from "./components/Dealers/Dealer"
 
 function App() {
   return (
@@ -13,7 +15,12 @@ function App() {
       {/* Route for Registration */}
       <Route path="/register" element={<RegisterPanel />} />
 
-    
+      {/* Route for Dealers */}
+      <Route path="/dealers" element={<Dealers/>} />
+
+      {/* Route for Dealer REACT */}
+      <Route path="/dealer/:id" element={<Dealer/>} />    <Route path="/dealer/:id" element={<Dealer/>} />
+
     </Routes>
   );
 }
